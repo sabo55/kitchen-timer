5503efe0a79ecb74f34b2c4e700b738cdeeca854
sabo55 <kidd_and_penny@yahoo.co.jp>
2026-01-21 20:28:03 +0900

    Fix

--- src/components/TimerBoard.jsx
+++ src/components/TimerBoard.jsx
@@ -358,7 +358,7 @@ const floatPanel = {
       }}>
         {visibleIndices.map((pos) => (
           <div
-            key={pos}
+            key={(slots[pos] ?? pos)}
             style={{
               width: "100%",
               position: "relative",
